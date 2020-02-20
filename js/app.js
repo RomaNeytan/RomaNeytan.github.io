97470955fd38d1ff1a8ebfe887090116db0687d2
RomaNeytan <57945586+RomaNeytan@users.noreply.github.com>
2020-02-20 18:24:06 +0300

    Проект готов к публицации

--- js/app.js
+++ js/app.js
@@ -10,4 +10,65 @@ $(function(){
         }
         
         ibg();
+
+    // Header fixed
+
+    let header = $("#header"),
+        introH = $("#intro").innerHeight(),
+        scrolloffsize = $(window).scrollTop();
+
+        CheckScroll(scrolloffsize,introH);
+    
+    $(window).on("scroll resize", function(){
+        scrolloffsize = $(this).scrollTop();
+        CheckScroll(scrolloffsize,introH);
+    });
+
+    function CheckScroll(scrolloffsize,introH){
+        if(scrolloffsize > introH){
+            header.addClass("fixed")
+        }
+
+        else{
+            header.removeClass("fixed");
+        }
+    }
+
+    CheckScroll(scrolloffsize,introH);
+
+    // Burger
+
+    $("#header-burger").on("click", function(){
+        $(this).toggleClass("active");
+
+        $("#header-menu").toggleClass("active");
+    });
+
+    // Smooth Scroll
+
+    $("[data-scroll]").on("click", function(event){
+        event.preventDefault();
+
+        let dataBlock = $(this).data("scroll"),
+            dataScroll = $(dataBlock).offset().top;
+
+        $("#header-burger").removeClass("active");
+        $("#header-menu").removeClass("active");
+
+        $("html,body").animate({
+            scrollTop: dataScroll - 140
+        },700);
+    });
+
+    // Slick slider
+
+    $("#reviews-slider").slick({
+        dots: true,
+        arrows:false,
+        fade:true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    });
 });
